03fc38e870535ec90016b29779eab96c66fcc713
Ankit Singh <ankitsinghmyself@gmail.com>
2024-11-05 22:07:19 +0530

    @swc/jest  add place of babel

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,7 @@ module.exports = {
     moduleNameMapper: {
       '\\.(scss|sass|css)$': 'identity-obj-proxy',
     },
+    transform: {
+      '^.+\\.(t|j)sx?$': ['@swc/jest'],
+    },
   };
--- src/lib/productService.ts
+++ src/lib/productService.ts
@@ -15,9 +15,11 @@ export async function getActiveProducts(): Promise<number> {
   });
   return activeProducts;
 }
+
 export async function getProducts() {
   return await prisma.products.findMany();
 }
+
 export async function addProduct(data: { name: string; details: string; price: number; img: string; quantity: number; isActive: boolean; authorId: string }) {
   return await prisma.products.create({ data });
 }
@@ -28,7 +30,6 @@ export async function updateProduct(id: string, data: { name: string; details: s
     data,
   });
 }
-
 
 export async function deleteProduct(id: string) {
   return await prisma.products.delete({
